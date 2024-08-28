bcc1faf66c9f14425328a981d0ef801c241b1b24
Avindi Obeyesekere <avindiobeyesekere1@gmail.com>
2024-08-28 20:52:35 +0530

    second mongo connection

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
-import express from "express";
+
+import mongoose from "mongoose";
 
 
+mongoose.connect("")
+
+const connect = require("./connect")
+const express = require("express")
+const cors = require("cors")
 const app = express();
 
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000!!");
+    console.log("Server is running on port 3000!!!");
 });
